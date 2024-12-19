e6a30e8ab5a76d78f98ca160f4b3da37dc0c74e5
Meteor0223 <144758462+Meteor0223@users.noreply.github.com>
2024-12-19 16:33:32 +0800

    week15

---
+++ week15/week15-1.cpp
@@ -0,0 +1,13 @@
+//week15-1.cpp UVA12250 not ok
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string hello;
+	int t=1;
+	while(cin>>hello){
+		if(hello=="#")break;
+		cout<<"Case"<<t<<":ENGLISH\n";
+		t++;
+	}
+}
